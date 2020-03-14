77ed644c76cf4f70b34603bc689bca363a83f4eb
Playhelpo <playhelp999md@gmail.com>
2020-03-14 13:45:03 +0100

    Update install_configs.sh
    fixed issuse autologin

--- install_configs.sh
+++ install_configs.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+#copy .zsh rc file to set automatic startx
 cp ./configs/.zshrc $HOME/.zshrc
+
+#copy .xinitrc file to set keyboard layout
 cp ./configs/.xinitrc $HOME/.xinitrc
 
+#create folder and copy override.conf to set autologin
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
 sudo cp ./configs/override.conf /etc/systemd/system/getty@tty1.service.d/
